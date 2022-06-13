1b45d7e1dcc8afb448858c1042b073d4e1c042f1
clifstone <47040820+clifstone@users.noreply.github.com>
2022-06-13 16:26:42 -0500

    newsletter popup and styles

--- footer.php
+++ footer.php
@@ -64,6 +64,190 @@
         })}})();
     </script>
 
+<div id="nwsltrpopup" class="nwsltrpopup">
+
+<div class="wrapper">
+    <div id="nwsltrpopup-close" class="closebtn">
+        <svg version="1.1" id="derfff" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 48 48" xml:space="preserve">
+            <style type="text/css">
+                .st0{fill:#FFFFFF;}
+            </style>
+            <polygon class="st0" points="48,3.7 44.3,0 24,20.3 3.7,0 0,3.7 20.3,24 0,44.3 3.7,48 24,27.7 44.3,48 48,44.3 27.7,24 "/>
+        </svg>
+    </div>
+
+    <div class="wrapper">
+        <figure class="nwsltr-thumb" style="background:url(https://www.getzone.com/wp-content/uploads/2021/09/newsletterimg.jpg) no-repeat 50% 50% / cover">
+            <div class="logo">
+                <img src="https://www.getzone.com/wp-content/uploads/2021/09/gz-logo-com-flat-black.svg" alt="" class="wp-image-59570" />
+            </div>
+            <div class="txt">Video Destination for Firearms & Hunting Enthusiasts</div>
+        </figure>
+        <div class="mcheads">
+            <p>Be in the Know. Join the GetZone Newsletter</p>
+            <p>Hottest Firearms &amp; Gear &#9656; Tips, Tricks & Skills &#9656; Shoot/Hunt Adventures &#9656; Manufacturer & 2A News &#9656; Deals &#9656; Exclusive Sweepstakes</p>
+        </div>
+        <div id="fonwsltrpop" class="nwslttr" style="position:relative;">
+            <div class="wrapper">
+                <form id="nwslttrfopop" method="post" action="https://enews.getzone.com/q/ws0cWalgPahogtXvLo6aBt5vaXfL6Wx6xMQkzQHMUUcUdnWKDEhcK4cGN" target="nwsifra" accept-charset="UTF-8">
+
+                    <input type="hidden" name="crvs" value="5d1x1Hb_7K6-IIbH8-z7M5wC8zZt6dBBVi9Lkj1r9nGmoEcyjfTRynSFOSH1FToCeRjz26McnD10OHYFo23dvw">
+
+                    <input type="text" name="ABC" size="10" maxlength="10" value="" tabindex="-1" autocomplete="off" style="float: left !important; position: absolute !important; left: -9000px !important; top: -9000px !important;">
+                    <input type="text" name="XYZ" size="10" maxlength="10" value="" tabindex="-1" autocomplete="off" style="float: left !important; position: absolute !important; left: -9000px !important; top: -9000px !important;">
+                    <input type="text" name="AtZ" size="10" maxlength="10" value="" tabindex="-1" autocomplete="off" style="float: left !important; position: absolute !important; left: -9000px !important; top: -9000px !important;">
+
+                    <div>
+                        <div style="color:var(--whitetext); background:var(--tertiary); padding:16px;">
+                            <div style="max-width:768px; margin:auto;">
+                                <div style="margin:0 0 8px 0; font-weight:700;">
+                                    <span id="nwslentpop">Get on the list. Enter your email address</span><span style="color: black"> *</span>
+                                </div>
+                                <input id="nwsltrinpop" type="text" name="email" size="40" maxlength="100" value="" style="border-radius:4px">
+
+                                <input type="text" name="NameF" size="10" maxlength="10" value="" tabindex="-1" autocomplete="off" style="float: left !important; position:absolute !important; left:-9000px !important; top: -9000px !important;">
+
+                                <input type="text" name="NameL" size="10" maxlength="10" value="" tabindex="-1" autocomplete="off" style="float: left !important; position:absolute !important; left:-9000px !important; top: -9000px !important;">
+
+                                <input type="text" name="Comment" size="10" maxlength="10" value="" tabindex="-1" autocomplete="off" style="float: left !important; position:absolute !important; left:-9000px !important; top: -9000px !important;">
+                            </div>
+                        </div>
+
+                        <div class="radiowrapper">
+                            <ul class="grid">
+                                <li style="font-weight:700;">
+                                    What interests you most? (Check all that apply)
+                                </li>
+                                <li>
+                                    <input style="width: auto; margin: 0 8px 0 0;" type="checkbox" name="CheckBox.Interests.Concealed Carry">
+                                    <input type="hidden" name="CheckBox.Interests.Concealed Carry" value="off">
+                                    <label>Concealed Carry</label>
+                                </li>
+                                <li>
+                                    <input style="width: auto; margin: 0 8px 0 0;" type="checkbox" name="CheckBox.Interests.Hunting">
+                                    <input type="hidden" name="CheckBox.Interests.Hunting" value="off">
+                                    <label>Hunting</label>
+                                </li>
+                                <li>
+                                    <input style="width: auto; margin: 0 8px 0 0;" type="checkbox" name="CheckBox.Interests.Handgun and accessories">
+                                    <input type="hidden" name="CheckBox.Interests.Handgun and accessories" value="off">
+                                    <label>Handgun and accessories</label>
+                                </li>
+                                <li>
+                                    <input style="width: auto; margin: 0 8px 0 0;" type="checkbox" name="CheckBox.Interests.personal defense">
+                                    <input type="hidden" name="CheckBox.Interests.personal defense" value="off">
+                                    <label>Home &amp; Personal Defense</label>
+                                </li>
+                                <li style="text-align:center">
+                                    <div style="font-size:11px; text-align:center; color:#999999; margin:0 auto 8px; max-width:480px;"><span>By giving you my email address, I am giving you permission to contact me for marketing and other purposes, as explained in the </span><a href="https://www.getzone.com/privacy-policy/" style="font-size:11px; color:#999999; text-decoration:underline">Privacy Policy</a></div>
+                                    <button id="newsltrsubpop" class="newsltrsubpop" type="submit">GET IT!</button>
+                                    <div id="dontshowagain" style="text-align:center"><span style="font-size:14px; color:#999999; padding:12px 16px 0; display:block; cursor:pointer;">Don't Show This Again</span></div>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+        <script>
+            window.addEventListener('load', function() {
+
+                var mcnws = document.getElementById('fonwsltrpop');
+                var mcfo = document.getElementById('nwslttrfopop');
+                var mcinput = document.getElementById('nwsltrinpop');
+                var mcent = document.getElementById('nwslentpop');
+
+                var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+                function popClose(){
+                    if (newspop.classList.contains('active')) {
+                        newspop.classList.remove('active');
+                    }
+                }
+
+                function createnwspxl() {
+                    var newsPxl = '<img src="//pd.trysera.com/c/14799086645428617223" width="1" height="1" style="display:none" />';
+                    document.body.insertAdjacentHTML('beforeend', newsPxl);
+                }
+
+                function createnwsifra(mcaction) {
+                    var nwsifra = '<iframe' + ' ' + 'src="' + mcaction + '" name="nwsifra" width="1" height="1" frameborder="0" scrolling="no" style="display:none"></iframe>';
+                    document.body.insertAdjacentHTML('beforeend', nwsifra);
+                    mcfo.submit();
+                }
+
+                function creategivethanks() {
+                    var gvthx = '<div style="position:absolute;top:0;left:0;bottom:0;right:0;display:flex;align-items:center;justify-content:center;background:rgba(0, 0,0, 0.9);"><div style="text-align:center; color:#FFFFFF;"><p style="font-size:32px;font-weight:700">Thank You!</p><p style="font-size:18px;font-weight:400">Your email has been submitted</p></div></div></div>';
+                    mcnws.insertAdjacentHTML('beforeend', gvthx);
+
+                    setTimeout(popClose, 1500);
+                }
+
+                function theyclicked(){
+                    localStorage.setItem('dontshow', true);
+                    popClose();
+                }
+
+                function newspxl(e) {
+
+                    e.preventDefault();
+
+                    if (mcinput.value.match(mailformat)) {
+                        var mcaction = e.target.form.action;
+                        mcent.removeAttribute('style');
+                        nwsltrinpop.removeAttribute('style');
+                        mcent.innerHTML = "Enter Your Email Address";
+                        creategivethanks();
+                        createnwsifra(mcaction);
+                        createnwspxl();
+                        localStorage.setItem('dontshow', true);
+                        return true;
+                    } else {
+                        console.log('invalid');
+                        mcent.style.color = '#000000';
+                        nwsltrinpop.style.border = '3px solid #000000';
+                        mcent.innerHTML = "Please Enter a Valid Email Address";
+                        return false;
+                    }
+
+                }
+                document.querySelector('.newsltrsubpop').addEventListener('click', newspxl);
+
+
+                var newspop = document.querySelector('#nwsltrpopup'),
+                    nwsClose = document.querySelector('#nwsltrpopup-close'),
+                    dontshowbtn = document.querySelector('#dontshowagain');
+
+                    if(!sessionStorage.getItem('sesh') && !localStorage.getItem('dontshow')){
+                        var locstore = localStorage.getItem('visnum');
+                        
+                        if(locstore === null){
+                            setTimeout(function(){
+                                localStorage.setItem('visnum', '1');
+                                newspop.classList.add('active');
+                            }, 15000);
+                        }
+                        
+                        if(locstore >= 1 && locstore < 3){
+                            locstore++;
+                            localStorage.setItem('visnum', locstore);
+                        }
+                        
+                        if(locstore === 3){
+                            localStorage.removeItem('visnum');
+                        }
+
+                        sessionStorage.setItem('sesh', true);
+                    }
+
+                    dontshowbtn.addEventListener('click', theyclicked);
+
+                    nwsClose.addEventListener('click', popClose);
+
+            });
+        </script>
+
     <?php wp_footer(); ?>
     
 </body>
